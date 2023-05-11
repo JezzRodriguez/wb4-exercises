149436fa45beec398da05cce4ee0bbd6eae0af58
JezzRodriguez <jrodriguez@appdev.yearup.org>
2023-05-11 19:35:41 -0400

    Exericise 1 page 2-16 Loops through an array and prints out the name using a fucntion

---
+++ ArrayScripts/my_family.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let myFamily = ["Jayce", "Kisha", "Jeff", "Jerel" ]
+
+function getFamilyNames (family) {
+let names = family.length
+for (let i = 0; i < names; i++) {
+    console.log(family[i]);
+    
+}
+}
+
+getFamilyNames(myFamily)
